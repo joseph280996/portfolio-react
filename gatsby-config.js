ae1a9ddcc7732a865e77b642a27652dba4a90681
Tung Pham <joseph.pham280996@gmail.com>
2024-05-12 22:56:46 -0400

    Update the core changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,12 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId,
+        trackingIds: [trackingId],
       },
     },
+    'gatsby-plugin-image',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -22,9 +23,9 @@ module.exports = {
         short_name: 'Agency',
         start_url: '/',
         background_color: '#ffffff',
-        theme_color: '#24b0bd',
+        theme_color: '#fed136',
         display: 'minimal-ui',
-        icon: 'content/assets/logo.png',
+        icon: 'content/assets/gatsby-icon.png',
       },
     },
     'gatsby-transformer-remark',
@@ -35,16 +36,20 @@ module.exports = {
         path: `${__dirname}/content`,
       },
     },
-    'gatsby-plugin-eslint',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-image',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
         path: `${__dirname}/content/assets/images`,
+      },
+    },
+    'gatsby-plugin-eslint',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-sharp',
+      options: {
+        checkSupportedExtensions: false,
       },
     },
     'gatsby-plugin-offline',
@@ -53,32 +58,34 @@ module.exports = {
       options: {
         additionalData: `@import "core.scss";`,
         sassOptions: {
-          includePaths: [`${__dirname}/src/style`],
+          includePaths: [path.resolve(__dirname, 'src/style')],
         },
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-prefetch-google-fonts",
-    //   options: {
-    //     fonts: [
-    //       {
-    //         family: "Montserrat",
-    //         variants: [400, 700],
-    //       },
-    //       {
-    //         family: "Kaushan+Script",
-    //       },
-    //       {
-    //         family: "Droid+Serif",
-    //         variants: [400, 700, "400italic", "700italic"],
-    //       },
-    //       {
-    //         family: "Roboto+Slab",
-    //         variants: [400, 100, 300, 700],
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: 'gatsby-omni-font-loader',
+      options: {
+        preconnect: ['https://fonts.gstatic.com'],
+        web: [
+          {
+            name: 'Droid Sans',
+            file: 'https://fonts.googleapis.com/css2?family=Droid+Sans&display=swap',
+          },
+          {
+            name: 'Montserrat',
+            file: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap',
+          },
+          {
+            name: 'Kaushan Script',
+            file: 'https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap',
+          },
+          {
+            name: 'Roboto Slab',
+            file: 'https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap',
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-plugin-i18n',
       options: {
